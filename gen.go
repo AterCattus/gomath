5b48ea709d30f242f5af11867e4ea182ffea593f
aaculovich <cat@ater.me>
2016-08-26 16:28:24 +0300

    add min/max for int types

---
+++ gen.go
@@ -0,0 +1,3 @@
+package gomath
+
+//go:generate go run math_gen/main.go -i math_gen/tmpl.go -o generated.go
---
+++ math_gen/tmpl.go
@@ -0,0 +1,15 @@
+package main
+
+func min_TYPENAME_(a, b _TYPE_) _TYPE_ {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max_TYPENAME_(a, b _TYPE_) _TYPE_ {
+	if a >= b {
+		return a
+	}
+	return b
+}
