051b8e0a82357a3fb76491ebf789cd8cca1235b1
aaculovich <cat@ater.me>
2016-09-02 16:34:12 +0300

    add Sign, Abs

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
 package gomath
 
-//go:generate go run math_gen/main.go -tmpl_int math_gen/tmpl_int.go -tmpl_common math_gen/tmpl_common.go -o generated.go
+//go:generate go run math_gen/main.go -tmpl_int math_gen/tmpl_int.go -tmpl_common math_gen/tmpl_common.go -tmpl_signed math_gen/tmpl_signed.go -o generated.go
---
+++ math_gen/tmpl_signed.go
@@ -0,0 +1,15 @@
+package main
+
+func Sign_TYPENAME_(a _TYPE_) _TYPE_ {
+	if a >= 0 {
+		return 0
+	}
+	return -1
+}
+
+func Abs_TYPENAME_(a _TYPE_) _TYPE_ {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
